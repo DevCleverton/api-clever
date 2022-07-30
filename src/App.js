24ac4e8dd8a995607d321e2d3d0442b9623ee5cb
Cleverton <devcleverton@mail.com>
2022-07-29 22:42:41 -0300

    up

--- src/App.js
+++ src/App.js
@@ -2,6 +2,21 @@
 import './App.css';
 
 function App() {
+
+  const options = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': '100b8fcb70msh4f262bd72374094p1207a7jsnaded30b50051',
+      'X-RapidAPI-Host': 'mdblist.p.rapidapi.com'
+    }
+  };
+  
+  fetch('https://mdblist.p.rapidapi.com/?s=jaws&m=show', options)
+    .then(response => response.json())
+    .then(response => console.log(response))
+    .catch(err => console.error(err));
+
+
   return (
     <div className="App">
       
